3be8ac0346cdfe13bad95dfbac19ce5297a66980
Brooks Jackson <90881109+bjaxqq@users.noreply.github.com>
2023-05-21 00:28:41 -0400

    Create TestFactorial.java

---
+++ Week 4/TestFactorial.java
@@ -0,0 +1,11 @@
+// Mark Hoffman 01/25/09
+//
+// Test recursive and iterative versions of Factorial number
+public class TestFactorial {
+    public static void main(String[] args) {
+        System.out.println("\nFactorial Number using Recursion\n");
+        System.out.println("Result:fact1(6) = " + Factorial.fact1(6));
+        System.out.println("\n\nFactorial Number using Iteration\n");
+        System.out.println("Result:fact2(6) = " + Factorial.fact2(6));
+    }
+}
